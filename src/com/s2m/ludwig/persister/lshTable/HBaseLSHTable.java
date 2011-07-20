b8e27979f750d8a307d0f85be79983861d4999e0
Elia Bruni <elia.bruni@gmail.com>
2011-07-20 23:17:25 +0200

    refactoring HBaseLSHTable

--- src/com/s2m/ludwig/persister/lshTable/HBaseLSHTable.java
+++ src/com/s2m/ludwig/persister/lshTable/HBaseLSHTable.java
@@ -2,30 +2,41 @@ package com.s2m.ludwig.persister.lshTable;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.util.Bytes;
+
+import com.carrotsearch.hppc.LongObjectOpenHashMap;
+import com.carrotsearch.hppc.ObjectObjectOpenHashMap;
 
 
 public class HBaseLSHTable {
 
 	private static final long BUFFERSIZE = 65536;
 	private static final byte[] SUMARRAYS = Bytes.toBytes("SumArrays");
-	
+
 	private HTable htable;
+
+	private List<Put> puts;
+	private List<Get> gets;
 
 	public HBaseLSHTable() throws IOException {
 		// TODO: pass to HTablePool
 		Configuration conf = HBaseConfiguration.create();
 		htable = new HTable(conf, "SumArrays");
 		htable.setWriteBufferSize(BUFFERSIZE);
+		puts = new ArrayList<Put>();
+		gets = new ArrayList<Get>();
 	}
 
-	public Put flushWord(long word, float[] sumArrays) throws IOException {
+	public void createSumArrayPut(long word, float[] sumArrays) throws IOException {
 		ByteBuffer buffer = ByteBuffer.allocate(sumArrays.length * 4);
 
 		for (float f : sumArrays) {
@@ -33,18 +44,43 @@ public class HBaseLSHTable {
 		}
 		buffer.flip();
 		byte[] rowkey = Bytes.toBytes(word);
-		Put put = new Put(rowkey);
+		Put p = new Put(rowkey);
 
-		put.add(SUMARRAYS, SUMARRAYS, buffer.array());
-		
-		return put;
+		p.add(SUMARRAYS, SUMARRAYS, buffer.array());
+		puts.add(p);
+
 	}
-	
-	
+
+
 	public void flushWords(List<Put> puts) throws IOException {
 		htable.put(puts);
+		puts = new ArrayList<Put>();
 	}
-	
 
+	public void createSumArrayGet(long word) {
+		Get g = new Get(Bytes.toBytes(word));
+		g.addColumn(SUMARRAYS, SUMARRAYS);
+		gets.add(g);
+	}
+
+	public LongObjectOpenHashMap<float[]> fetchSumArrays() throws IOException {
+		LongObjectOpenHashMap<float[]> sumArrays = new LongObjectOpenHashMap<float[]>();
+		Result[] results = htable.get(gets);
+		for (Result res : results) {
+			byte[] byteTerm = res.getRow();
+			long term = Bytes.toLong(byteTerm);
+			byte[] byteSumArray = res.getValue(SUMARRAYS, SUMARRAYS);
+
+			ByteBuffer buffer = ByteBuffer.wrap(byteSumArray);
+			float[] sumArray = new float[buffer.capacity()/4];
+			int pointer = 0;
+			while (buffer.hasRemaining()) {
+				sumArray[pointer] = buffer.getFloat();
+			}
+			sumArrays.put(term, sumArray);
+		}
+		
+		return sumArrays;
+	}
 
 }
